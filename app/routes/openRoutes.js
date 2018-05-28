bc732a158f4ac6ac1fe652dacfe9d2309d9f2451
Aleksey Tomilov <suphuilo@gmail.com>
2018-05-28 18:01:56 +0800

    openapi

--- app/routes/openRoutes.js
+++ app/routes/openRoutes.js
@@ -2,13 +2,15 @@ const Router = require('koa-router');
 const KoaBody = require('koa-body');
 
 const {getContent} = require('../controllers/openApiController');
+const {register} = require('../controllers/registration');
 
 const router = new Router({
   prefix: '/openApi'
 });
 
 router
-  .post('/get', KoaBody(), getContent);
+  .post('/get', KoaBody(), getContent)
+  .post('/t', KoaBody(), register);
 
 module.exports = {
   openRoutes() { return router.routes(); },
